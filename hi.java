f1b1dd96febfb32c70bc0662a680ea2371bd40e4
Ubuntu <ubuntu@ip-172-31-11-247.ap-south-1.compute.internal>
2023-05-12 13:49:17 +0000

    modified output

--- hi.java
+++ hi.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Lets learn Jenkins!!! "); 
+        System.out.println("Hello, World! Welcome to Jenkins training !!! "); 
     }
 }
